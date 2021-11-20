5812d8ebe925c6dd154385351dd30fad2cd302c1
MischaGe <gemsa.michael@gmail.com>
2021-11-20 22:40:35 +0100

    Initial Commit

---
+++ FitnessTimer/FitnessTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTimerApp.swift
+//  FitnessTimer
+//
+//  Created by Michael Gemsa on 20.11.21.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
